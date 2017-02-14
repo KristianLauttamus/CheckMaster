5790433bb8b3f3bb86f1280c08dbbf92800ae406
KristianLauttamus <kristian.lauttamus@gmail.com>
2017-02-14 13:04:17 +0200

    RestrictionEditingForm WIP

---
+++ CheckMaster/RestrictionEditingForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CheckMaster
+{
+    partial class RestrictionEditingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.moduleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(13, 282);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(605, 23);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Module:";
+            // 
+            // moduleLabel
+            // 
+            this.moduleLabel.AutoSize = true;
+            this.moduleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moduleLabel.Location = new System.Drawing.Point(57, 12);
+            this.moduleLabel.Name = "moduleLabel";
+            this.moduleLabel.Size = new System.Drawing.Size(47, 15);
+            this.moduleLabel.TabIndex = 2;
+            this.moduleLabel.Text = "label2";
+            // 
+            // RestrictionEditingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 317);
+            this.Controls.Add(this.moduleLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.closeButton);
+            this.Name = "RestrictionEditingForm";
+            this.Text = "RestrictionEditingForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label moduleLabel;
+    }
+}
---
+++ CheckMaster/RestrictionEditingForm.cs
@@ -0,0 +1,40 @@
+﻿using CheckMaster.SuccessModules;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckMaster
+{
+    public partial class RestrictionEditingForm : Form
+    {
+        private Modules.Module module;
+        private SuccessModule successModule;
+
+        public RestrictionEditingForm(Modules.Module module)
+        {
+            this.module = module;
+            InitializeComponent();
+
+            this.moduleLabel.Text = module.ToString();
+        }
+
+        public RestrictionEditingForm(SuccessModule successModule)
+        {
+            this.successModule = successModule;
+            InitializeComponent();
+
+            this.moduleLabel.Text = successModule.ToString();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
